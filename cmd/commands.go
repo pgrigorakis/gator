770911e7fd17f8ca703fe9742babec1c4246329a
Panagiotis Grigorakis <p.grigorakis@gmail.com>
2026-01-19 22:53:46 +0100

    added login handler, run and register commands functionality

---
+++ cmd/commands.go
@@ -0,0 +1,56 @@
+package main
+
+import {
+	"fmt"
+	"github.com/pgrigorakis/gator/internal/config"
+}
+
+type state struct {
+	cfg *Config
+}
+
+type command struct {
+	name string
+	args []string
+}
+
+type commands struct {
+	commandMap map[string]func(*state, command) error
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return fmt.Errorf("no username")
+	}
+
+	err := s.cfg.SetUser(cmd.args[0])
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("User has been set to: %v", cmd.args[0])
+
+	return nil
+
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	handler, exists := c.commandMap[cmd.name]
+	if !exists {
+		return fmt.Errorf("unknown command: %v", cmd.name)
+	}
+
+	err := handler(s, cmd)
+	if err != nil {
+		return fmt.Errorf("error: %v", err)
+	}
+
+	return nil
+}
+
+func (c *commands) register(name string, f func(s *state, cmd command) error) {
+	_, exists := c.commandMap[name]
+	if !exists {
+		c.commandMap[name] = f
+	}
+}
