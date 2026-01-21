a22947dfa4013e10a8a53527c16cfd692178c8e8
Panagiotis Grigorakis <p.grigorakis@gmail.com>
2026-01-22 00:11:51 +0100

    added database logic and registered/run register command

--- main.go
+++ main.go
@@ -1,26 +1,34 @@
 package main
 
+import _ "github.com/lib/pq"
 import (
+	"database/sql"
 	"github.com/pgrigorakis/gator/internal/config"
+	"github.com/pgrigorakis/gator/internal/database"
 	"log"
 	"os"
 )
 
 type state struct {
+	db  *database.Queries
 	cfg *config.Config
 }
 
 func main() {
+
 	cfg, err := config.Read()
 	if err != nil {
 		log.Fatalf("error reading config: %v", err)
 	}
 
-	programState := &state{cfg: &cfg}
+	db, err := sql.Open("postgres", cfg.DBURL)
+	dbQueries := database.New(db)
+
+	programState := &state{db: dbQueries, cfg: &cfg}
 
 	cmds := &commands{commandMap: make(map[string]func(*state, command) error)}
 	cmds.register("login", handlerLogin)
-
+	cmds.register("register", handlerRegister)
 	userArgs := os.Args
 	if len(userArgs) < 2 {
 		log.Fatal("error: no command entered")
