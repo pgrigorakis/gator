0ce8becdeef697f7ff9fd79e561f0eecbe8bddca
Panagiotis Grigorakis <p.grigorakis@gmail.com>
2026-01-22 00:12:34 +0100

    added handlerRegister func to register user and added functionality to handlerLogin to check if user exists

--- handler_user.go
+++ handler_user.go
@@ -1,18 +1,68 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/pgrigorakis/gator/internal/database"
+)
 
 func handlerLogin(s *state, cmd command) error {
-	if len(cmd.args) == 0 {
-		return fmt.Errorf("no username")
+	if len(cmd.args) != 1 {
+		return fmt.Errorf("usage: %s <name>", cmd.name)
 	}
 
-	err := s.cfg.SetUser(cmd.args[0])
+	ctx := context.Background()
+
+	_, err := s.db.GetUser(ctx, cmd.args[0])
+	if err != nil {
+		return errors.New("no such user exists")
+	}
+	err = s.cfg.SetUser(cmd.args[0])
 	if err != nil {
 		return err
 	}
 
 	fmt.Printf("User has been set to: %v\n", cmd.args[0])
+
+	return nil
+
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.args) != 1 {
+		return fmt.Errorf("usage: %s <name>", cmd.name)
+	}
+	ctx := context.Background()
+
+	now := time.Now()
+
+	_, err := s.db.GetUser(ctx, cmd.args[0])
+	if err == nil {
+		return errors.New("user exists already")
+	} else {
+		registeredUser, err := s.db.CreateUser(ctx, database.CreateUserParams{
+			ID:        uuid.New(),
+			CreatedAt: now,
+			UpdatedAt: now,
+			Name:      cmd.args[0],
+		})
+		if err != nil {
+			return err
+		}
+
+		err = s.cfg.SetUser(cmd.args[0])
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("User has been set to: %v\n", cmd.args[0])
+		log.Printf("\nid: %v,\ncreated at: %v,\nupdated at: %v,\nname: %v\n", registeredUser.ID.String(), registeredUser.CreatedAt, registeredUser.UpdatedAt, registeredUser.Name)
+	}
 
 	return nil
 
