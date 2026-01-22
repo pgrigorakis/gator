44a2f53ed04997304c8fa123b0c09ee6eda5fbfb
Panagiotis Grigorakis <p.grigorakis@gmail.com>
2026-01-22 21:37:17 +0100

    reset now resets the feeds table too

--- handler_reset.go
+++ handler_reset.go
@@ -6,12 +6,16 @@ import (
 )
 
 func handlerResetDB(s *state, cmd command) error {
-	ctx := context.Background()
-
-	err := s.db.DeleteUsers(ctx)
+	err := s.db.DeleteUsers(context.Background())
 	if err != nil {
 		return fmt.Errorf("couldn't delete users %v", err)
 	}
+
+	err = s.db.DeleteFeeds(context.Background())
+	if err != nil {
+		return fmt.Errorf("couldn't delete users %v", err)
+	}
+
 	fmt.Println("Database reset successful.")
 	return nil
 }
