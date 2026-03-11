bfd9447d03a2d481c5054e9a9b99a6ce385fd638
Panagiotis Grigorakis <p.grigorakis@gmail.com>
2026-03-11 22:56:00 +0100

    modified scrapeFeeds to save posts into db

--- handler_agg.go
+++ handler_agg.go
@@ -2,28 +2,76 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"log"
+	"strings"
 	"time"
 
+	"github.com/google/uuid"
+	"github.com/pgrigorakis/gator/internal/database"
 	"github.com/pgrigorakis/gator/internal/rss"
 )
 
-func scrapeFeeds(s *state) {
+func parseTime(pubDate string) (time.Time, error) {
+	formats := []string{
+		time.RFC1123Z,
+		time.RFC1123,
+		// add others if needed
+	}
+	for _, format := range formats {
+		t, err := time.Parse(format, pubDate)
+		if err == nil {
+			return t, nil
+		}
+	}
+	return time.Time{}, fmt.Errorf("could not parse time: %s", pubDate)
+}
 
-	nextFeed, err := s.db.GetNextFeedToFetch(context.Background())
+func addPost(s *state, nextFeed database.Feed, item rss.RSSItem, ctx context.Context) error {
+	parsedTime, parseErr := parseTime(item.PubDate)
+	if parseErr != nil {
+		log.Printf("could not parse publication time for %s: %v", item.Title, parseErr)
+	}
+	_, err := s.db.CreatePost(ctx, database.CreatePostParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Title:     item.Title,
+		Url:       item.Link,
+		Description: sql.NullString{
+			String: item.Description,
+			Valid:  item.Description != "",
+		},
+		PublishedAt: sql.NullTime{
+			Time:  parsedTime,
+			Valid: parseErr == nil,
+		},
+		FeedID: nextFeed.ID,
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func scrapeFeeds(s *state) {
+	ctx := context.Background()
+	nextFeed, err := s.db.GetNextFeedToFetch(ctx)
 	if err != nil {
 		log.Println("could not get next feed to fetch:", err)
 		return
 	}
 
-	_, err = s.db.MarkFeedFetched(context.Background(), nextFeed.ID)
+	_, err = s.db.MarkFeedFetched(ctx, nextFeed.ID)
 	if err != nil {
 		log.Printf("could not mark feed %s as fetched: %v", nextFeed.Name, err)
 		return
 	}
 
-	feed, err := rss.FetchFeed(context.Background(), nextFeed.Url)
+	feed, err := rss.FetchFeed(ctx, nextFeed.Url)
 	if err != nil {
 		log.Printf("could not get feed %s to fetch: %v", nextFeed.Url, err)
 		return
@@ -31,6 +79,13 @@ func scrapeFeeds(s *state) {
 
 	for _, item := range feed.Channel.Item {
 		fmt.Printf("Found post: [%s] %s\n", feed.Channel.Description, item.Title)
+		err = addPost(s, nextFeed, item, ctx)
+		if err != nil {
+			if strings.Contains(err.Error(), "duplicate key") {
+				continue
+			}
+			log.Printf("could not create post %s: %v", item.Title, err)
+		}
 	}
 
 	log.Printf("Feed %s collected, %v posts found", nextFeed.Name, len(feed.Channel.Item))
